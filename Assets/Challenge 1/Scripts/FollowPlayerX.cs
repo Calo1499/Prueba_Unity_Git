19e8275db8d5f5fdb25e482a3e23744ffe5eaa59
ZerolCamb <zerolcamb@outlook.com>
2021-10-09 11:33:26 -0500

    Primer Commit
    Primer Commit para la prueba de Unity en GitHub

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(35, 4, 16);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Place the camera position on top of the player's position
+        transform.position = player.transform.position + offset;
+    }
+}
